14028e121e2a0e309e03c2a2ff972d1f884d3b8f
RaulVillarraso <raulvillarraso@gmail.com>
2023-11-20 10:17:07 +0000

    landing static page done, need to make it responsive

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,29 +1,51 @@
 import Header from "./Header/Header"
 import "./Home.css"
-import { Box, Typography } from '@mui/material'
+import { Box, Grid, Typography } from '@mui/material'
 
 function Home() {
     return (
         <>
             <Header />
             <Box className="portrait">
-                <Box className="portraitImage">
-                    <img src="https://placehold.co/700x400" />
-                </Box>
-                <Box className="portraitText">
-                    <Typography variant="subtitle1">Reboost Academy</Typography>
-                    <Typography variant="body1">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Mauris congue rhoncus finibus. Suspendisse potenti. Ut
-                        lobortis felis efficitur lectus pulvinar aliquet. Etiam
-                        a odio eros. Praesent arcu nulla, tincidunt nec velit
-                        vel, interdum consequat massa. Suspendisse mollis purus
-                        ut nisi molestie, sit amet rhoncus ligula vehicula.
-                        Pellentesque mollis lacus et eleifend bibendum. Donec
-                        vulputate mauris at tortor tincidunt, vel euismod massa
-                        posuere. 
-                    </Typography>
-                </Box>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} lg={8}>
+                        <Box className="portraitImage">
+                            <img src="https://placehold.co/700x400" />
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} lg={4}>
+                        <Box className="portraitText">
+                            <Typography
+                                variant="subtitle1"
+                                sx={{ margin: "0 24px" }}
+                            >
+                                Reboost Academy
+                            </Typography>
+                            <Typography
+                                variant="body1"
+                                sx={{ margin: "0 24px" }}
+                            >
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit. Mauris congue rhoncus finibus.
+                                Suspendisse potenti. Ut lobortis felis efficitur
+                                lectus pulvinar aliquet. Etiam a odio eros.
+                                Praesent arcu nulla, tincidunt nec velit vel,
+                                interdum consequat massa. Suspendisse mollis
+                                purus ut nisi molestie, sit amet rhoncus ligula
+                                vehicula. Pellentesque mollis lacus et eleifend
+                                bibendum. Donec vulputate mauris at tortor
+                                tincidunt, vel euismod massa posuere.
+                            </Typography>
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Box>
+            <Box className="imageList">
+                <img src="https://placehold.co/140"/>
+                <img src="https://placehold.co/140"/>
+                <img src="https://placehold.co/140"/>
+                <img src="https://placehold.co/140"/>
+                <img src="https://placehold.co/140"/>
             </Box>
         </>
     )
