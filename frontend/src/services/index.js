0f0ed89df7a949052b4a5e967780312ddb76d7d2
adrianaadd <adriansalvadortrabajos@gmail.com>
2023-11-28 15:57:46 +0000

    projectz2

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -2,12 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: "http://localhost:3000/api",
-<<<<<<< HEAD
-    headers: {
-        authorization: localStorage.token,
-    },
-=======
->>>>>>> main
 })
 
 export default api
