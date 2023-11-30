1ba751d58b047d97eaca36a69c7ac43a61794103
RaulVillarraso <raulvillarraso@gmail.com>
2023-11-30 10:16:03 +0000

    authorization issues solved

--- backend/api/routes/booking.router.js
+++ backend/api/routes/booking.router.js
@@ -2,9 +2,9 @@ const router = require('express').Router()
 const { getClassesAndTeachersForBookingDate,getAllBookings, getOneBooking, createBooking, updateBooking, deleteBooking } = require('../controllers/booking.controller')
 const { checkAuth, checkAdmin } = require('../middleware')
 
-router.get('/', checkAuth, checkAdmin,  getAllBookings)
-router.get("/:id", checkAuth, checkAdmin, getOneBooking);
-router.get('/clase/classroom/:id', checkAuth, checkAdmin, getClassesAndTeachersForBookingDate)
+router.get('/', checkAuth,  getAllBookings)
+router.get("/:id", checkAuth, getOneBooking);
+router.get('/clase/classroom/:id', checkAuth, getClassesAndTeachersForBookingDate)
 router.post("/",  checkAuth, checkAdmin,  createBooking);
 router.put("/:id",  checkAuth, checkAdmin, updateBooking);
 router.delete("/:id",  checkAuth, checkAdmin, deleteBooking);
