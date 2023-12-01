1c6ba91902d764a5b2c8f6fdc1170a2ab1177b45
RaulVillarraso <raulvillarraso@gmail.com>
2023-12-01 12:21:46 +0000

    api dork i am

--- frontend/src/services/index.js
+++ frontend/src/services/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://reboost-academy.onrender.com/",
+    baseURL: "https://reboost-academy.onrender.com/api",
 })
 
 export default api
